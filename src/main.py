40c3140e1b9d2bb80de294f24badba06c56d1834
mockupname <mockup.name.sec@gmail.com>
2019-03-28 13:28:11 +0100

    version 0.2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import sys
 import time
+from version import __version__
 
 with open("executed_{}.txt".format(time.time()), "w+") as file:
+    file.write("version: {}\n".format(__version__))
     file.write("argv len: {}\n\n".format(len(sys.argv)))
     file.write("\n".join(sys.argv))
